b1b6fbc2f69837bf76bfc728f3f38fa2e29a651c
qpeyba <145938039+qpeyba@users.noreply.github.com>
2026-03-30 15:10:00 +0300

    refactor message interface & implement message handling for slide number changing

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,1 +1,23 @@
-export {};
+import { Message, MessageType } from '../types';
+
+function handleMessage(
+  message: Message,
+  sender: chrome.runtime.MessageSender,
+  sendResponse: (response?: any) => void
+): boolean | void {
+  if (message.type === MessageType.SLIDE_NUMBER_CHANGED) {
+    chrome.storage.local.set({
+      lastLiveSlide: message.slideNumber,
+    });
+  } else if (message.type === MessageType.GET_LAST_SLIDE) {
+    chrome.storage.local.get(['lastLiveSlide'], (result) => {
+      sendResponse({ slideNumber: result.lastLiveSlide || null });
+    });
+    return true;
+  }
+}
+
+chrome.runtime.onMessage.addListener(handleMessage);
+
+
+
