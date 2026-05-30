b2abcaadd3bd46b08b65ce2083bc2509ce5701ad
qpeyba <145938039+qpeyba@users.noreply.github.com>
2026-05-30 15:49:12 +0300

    disable debugging

--- src/content/content.ts
+++ src/content/content.ts
@@ -6,7 +6,7 @@ import {
 } from './slide';
 import { reduceObservedSlide } from './slide-state';
 
-const DEBUG = true;
+const DEBUG = false;
 
 let followPresenter = true;
 let localSlideNumber: number | null = null;
@@ -214,10 +214,6 @@ function initialize(): void {
   setupObserver();
 }
 
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', initialize);
-} else {
-  initialize();
-}
+initialize();
 
 export { getSlideImage, getCurrentSlideNumber, changeSlide, setSlideNumber };
